60bf6b9d62a6767868381c55acd6e56cd74fc3ef
Marco Tsui <marcotsui2003@yahoo.com>
2016-06-29 10:27:21 -0400

    add carts controller and show view

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,19 @@
 class CartsController < ApplicationController
 
+  def show
+    @current_cart = Cart.find_by(id: params[:id])
+  end
+
+  def checkout
+    @cart = Cart.find_by(id: params[:id])
+    @cart.items.each do  |item|
+      line_item = LineItem.find_by(item_id: item.id)
+      item.inventory -= line_item.quantity
+      item.save
+    end
+    @cart.update(status: "submitted")
+    current_user.update(current_cart: nil)
+    redirect_to cart_path(@cart)
+  end
+
 end
