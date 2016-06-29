7b405dc846d1149342b6979d91e5e71dd9d6d3f3
Marco Tsui <marcotsui2003@yahoo.com>
2016-06-29 10:31:11 -0400

    add stores controller and index view

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,9 @@
 class StoreController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   def index
+    @categories =  Category.all
+    @items = Item.available_items
   end
 
 
